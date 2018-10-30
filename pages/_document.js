dc199247e22a67d0e1ef5e8da4d4d1e3cdcea891
jarindr <jarindr23@gmail.com>
2018-10-30 23:05:45 +0100

    add responsive meta tag

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,7 @@ export default class MyDocument extends Document {
       <html>
         <title>OmiseGO Explorer</title>
         <Head>{this.props.styleTags}</Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href={require('../statics/images/favicon.png')} type='image/x-icon' />
         <link rel='shortcut icon' href={require('../statics/images/favicon.png')} type='image/x-icon' />
         <link
